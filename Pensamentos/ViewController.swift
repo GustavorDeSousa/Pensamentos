b264210ed6934362c4ef99f24695c77998bf834d
Gustavo De Sousa <gustavodesousa@gustavos-macbook-pro.local>
2018-09-24 03:54:21 -0300

    Initial Commit

---
+++ Pensamentos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pensamentos
+//
+//  Created by Gustavo De Sousa on 24/09/18.
+//  Copyright © 2018 Gustavo De Sousa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
